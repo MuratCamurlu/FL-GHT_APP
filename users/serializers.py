73f8c2fe36ba3ef9eebc75cae13f24e38a22dac7
MuratÇamurlu <mcamurlu1923@gmailcom>
2023-01-05 12:55:24 +0300

    path

--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 from rest_framework.validators import UniqueValidator
 from django.contrib.auth.password_validation import validate_password
+
+
+
 class RegisterSerializer(serializers.ModelSerializer):
 	email = serializers.EmailField(required=True, validators=[UniqueValidator(queryset=User.objects.all())])
  
