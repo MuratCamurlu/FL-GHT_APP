c796d33d6865dbfded809f997782df7d3e8c4c88
MuratÇamurlu <mcamurlu1923@gmail.com>
2023-01-06 17:06:44 +0300

    filter

--- flight/serializers.py
+++ flight/serializers.py
@@ -24,9 +24,26 @@ class PassengerSerializer(serializers.ModelSerializer):
         fields = "__all__"
         
         
-class   ReservationSerializer(serializers.ModelSerializer):
-    passenger=PassengerSerializer(many=True)
+class ReservationSerializer(serializers.ModelSerializer):
+
+    passenger = PassengerSerializer(many=True)
+    flight = serializers.StringRelatedField()
+    flight_id = serializers.IntegerField()
+    user=serializers.StringRelatedField()
     class Meta:
         model = Reservation
-        fields = ("id","flight","passenger")
-            
+        fields = ("id", "flight", "flight_id","user", "passenger")
+        
+        
+    def create(self, validated_data):
+ 
+        passenger_data = validated_data.pop("passenger")
+        validated_data["user_id"] = self.context["request"].user.id
+        reservation = Reservation.objects.create(**validated_data)
+        
+        for passenger in passenger_data:
+            pas = Passenger.objects.create(**passenger)
+            reservation.passenger.add(pas)
+        
+        reservation.save()
+        return reservation
